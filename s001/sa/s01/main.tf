d83b1fe3734510140643d11ab5267410ecba3a5b
Praveen <fattesinha.sawant@outlook.com>
2022-09-25 16:50:28 +0530

    re-arranged folder structure

--- s001/sa/s01/main.tf
+++ s001/sa/s01/main.tf
@@ -20,3 +20,11 @@ module "resource_group" {
   zone = var.zone
 }
 
+# storage account module created for storing the state file for all other resources
+module "storage_account" {
+  source = "git@github.com:praveen2391/humana-tf-modules.git//storage-account"
+  location = module.resource_group.location
+  resource_group_name = module.resource_group.resource_group_name
+  account_replication_type = "GRS"
+  account_tier = "Standard"
+}
