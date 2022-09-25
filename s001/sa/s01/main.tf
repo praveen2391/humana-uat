de435d7ff9719435e14a760e8f2a2bc19a33ee52
Praveen <fattesinha.sawant@outlook.com>
2022-09-25 19:32:46 +0530

    terraform format on s001 resources

--- s001/sa/s01/main.tf
+++ s001/sa/s01/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "1.0.11"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "2.99.0"
     }
   }
@@ -13,24 +13,24 @@ provider "azurerm" {
 }
 
 module "resource_group" {
-  source = "git@github.com:praveen2391/humana-tf-modules.git//resource-group"
+  source   = "git@github.com:praveen2391/humana-tf-modules.git//resource-group"
   location = var.location
-  project = var.project
-  lob = var.lob
-  zone = var.zone
+  project  = var.project
+  lob      = var.lob
+  zone     = var.zone
 }
 
 # storage account module created for storing the state file for all other resources
 module "storage_account" {
-  source = "git@github.com:praveen2391/humana-tf-modules.git//storage-account"
-  location = module.resource_group.location
-  resource_group_name = module.resource_group.name
+  source                   = "git@github.com:praveen2391/humana-tf-modules.git//storage-account"
+  location                 = module.resource_group.location
+  resource_group_name      = module.resource_group.name
   account_replication_type = "GRS"
-  account_tier = "Standard"
+  account_tier             = "Standard"
 }
 
 module "storage_account_container" {
-  source = "git@github.com:praveen2391/humana-tf-modules.git//storage-account-container"
-  container_name = var.storage_account_container_name
+  source               = "git@github.com:praveen2391/humana-tf-modules.git//storage-account-container"
+  container_name       = var.storage_account_container_name
   storage_account_name = module.storage_account.name
 }
--- s001/sa/s01/variables.tf
+++ s001/sa/s01/variables.tf
@@ -2,4 +2,4 @@ variable "location" {}
 variable "zone" {}
 variable "project" {}
 variable "lob" {}
-variable storage_account_container_name {}
+variable "storage_account_container_name" {}
